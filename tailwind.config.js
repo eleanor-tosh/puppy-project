65810b410c98fe26ffe02f9622eb2181e247ce27
eleanor-tosh <102724780+eleanor-tosh@users.noreply.github.com>
2022-11-28 23:56:44 +1300

    nav bar

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,14 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
   content: ['./index.html', './client/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
       colors: {
-        blue: '#O477BF',
-        grey: '#9CA1A6',
-        red: '#D90404',
-        white: '#F2F2F2',
+        // blue: '#O477BF',
+        // grey: '#9CA1A6',
+        // red: '#D90404',
+        // white: '#F2F2F2',
       },
       fontFamily: {
         heading: ['abril-text', 'serif'],
