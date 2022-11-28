a5e1738b5fcbcec3a56ea4b53dd7b5abd09cc821
eleanor-tosh <102724780+eleanor-tosh@users.noreply.github.com>
2022-11-28 17:20:28 +1300

    getting tailwind up and running

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html', './client/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        blue: '#O477BF',
+        grey: '#9CA1A6',
+        red: '#D90404',
+        white: '#F2F2F2',
+      },
+      fontFamily: {
+        heading: ['abril-text', 'serif'],
+        body: ['source-sans-pro', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
