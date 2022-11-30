0009c244ad56007d9a5eefdbc9811a591aeab1ec
eleanor-tosh <102724780+eleanor-tosh@users.noreply.github.com>
2022-11-30 22:55:37 +1300

    social database

---
+++ server/migrations/20221130092700_social.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema.createTable('social', function (table) {
+    table.increments('social_id').primary()
+    table.string('task')
+    table.string('task_notes')
+  })
+}
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.schema.dropTable('social')
+}
